0faff9292f0adbf75b55e99094339d0e69ef5d07
James Messinger <james.messinger@gmail.com>
2017-10-20 15:51:28 -0500

    Added tests for some specific demo data

--- test/specs/findCharacters.spec.js
+++ test/specs/findCharacters.spec.js
@@ -16,8 +16,11 @@ describe('Find characters', () => {
       .get('/characters')
       .then(res => {
         let characters = assert.isSuccessfulResponse(res, 200);
-        characters.should.be.an('array').with.lengthOf(28);
+        characters.should.be.an('array').with.lengthOf(30);
         characters.forEach(assert.isValidCharacter);
+        characters.should.satisfy(list => list.find(character => character.name === 'The Fantastic Four Spaces'));
+        characters.should.satisfy(list => list.find(character => character.name === 'The Feature Creep'));
+        characters.should.satisfy(list => list.find(character => character.name === 'The Incredible MVP'));
       });
   });
 
