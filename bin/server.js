03daa0e5798dc48a1114cc32920a8da35074c67e
James Messinger <james.messinger@gmail.com>
2017-10-22 21:36:19 -0500

    ESLint auto-fixes

--- bin/server.js
+++ bin/server.js
@@ -25,7 +25,7 @@ api.use(cors());
 api.use(bodyParser.json());
 api.use(mockApiGateway);
 api.listen(8080, () => {
-  console.log('The Super Tech Heroes API is now running at http://localhost:8080')
+  console.log('The Super Tech Heroes API is now running at http://localhost:8080');
 
   if (process.env.AWS_DYNAMODB_ENDPOINT) {
     console.log(`The Super Tech Heroes database is now running at ${process.env.AWS_DYNAMODB_ENDPOINT}/shell`);
