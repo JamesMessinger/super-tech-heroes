6f1fa0074a89ad3874035a294095e9df75a0bc73
James Messinger <james.messinger@gmail.com>
2017-10-29 09:20:49 -0500

    Added support for Postman Collection v2.0 API definition format.  Also moved the "schemas" folder from "lib" to the root folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # ==================================================================================================
-# This script builds the API schemas in the "lib/schemas" folder.  The Swagger 2.0 schema is considered
+# This script builds the API schemas in the "schemas" folder.  The Swagger 2.0 schema is considered
 # the "master" schema from which all other schemas are generated.
 # ==================================================================================================
 
@@ -8,6 +8,8 @@
 set -o errexit -o nounset -o pipefail
 
 echo Building API schemas...
-swagger2openapi --yaml lib/schemas/swagger.yaml > lib/schemas/openapi.yaml
-yaml2json --pretty lib/schemas/swagger.yaml > lib/schemas/swagger.json
-yaml2json --pretty lib/schemas/openapi.yaml > lib/schemas/openapi.json
+swagger2openapi --yaml schemas/swagger.yaml > schemas/openapi.yaml
+yaml2json --pretty schemas/swagger.yaml > schemas/swagger.json
+yaml2json --pretty schemas/openapi.yaml > schemas/openapi.json
+
+echo Done building
